2d38ca1fa1117741564052e0ba8022887b12d032
Simon Roth <e11907576@student.tuwien.ac.at>
2024-01-29 17:03:38 +0100

    added classification and line detection to cv node listener

--- cv/src/listener/image_listener.py
+++ cv/src/listener/image_listener.py
@@ -3,6 +3,12 @@
 import rospy
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
+import sys
+import os
+parent_dir = os.path.dirname(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
+sys.path.append(parent_dir)
+from ..image_processing.detect_line import detect_lines
+from ..image_processing.detect_victim import classify_image
 import cv2
 #from PIL import Image
 
@@ -15,8 +21,9 @@ def callback(data):
     #cv2.imshow("Image window", frame)
     #cv2.waitKey(3)
     #save image
-    cv2.imwrite('lastframe.png', frame)
-
+    #cv2.imwrite('lastframe.png', frame)
+    print(classify_image(frame))
+    detect_lines(frame)
 def listener_node():
     # Initialize the ROS node
     rospy.init_node('imagelistener', anonymous=True)
