6002b23d4e2f652007a94d314b2577f8b3b8a149
Peter V <128387629+VrabelPeter@users.noreply.github.com>
2024-01-31 22:28:15 +0100

    refactor and add some comments

--- battery/src/battery_node.py
+++ battery/src/battery_node.py
@@ -12,28 +12,28 @@ BATTERY_DEFAULT_RATE = 0.20  # Send every 5 seconds
 class BatterySubscriber:
 
     def start(self):
-        pub = rospy.Publisher('/battery/return_signal', Bool, queue_size=10)
+        pub = rospy.Publisher('/battery/return_signal', Bool, queue_size=10) # Bool is the message type
         rate = rospy.Rate(self._rate)
+        message = Bool() # create a message
         while not rospy.is_shutdown():
-            message = Bool()
             message.data = True if self._battery_status < BATTERY_THRESHOLD_IN_PERCENT else False
             pub.publish(message)
             rospy.loginfo("Publishing %s return signal", message.data)
             rate.sleep()  # wait according to the publishing rate
 
-    def battery_status_callback(self, data: Int32):
+    def battery_status_callback(self, data: Int32): # never call this func. yourself, called when msg arrives
         rospy.loginfo("battery at %s%%", data.data)
-        self._battery_status = data.data
+        self._battery_status = data.data # keep as short as possible, since interrupts the execution flow
 
     def __init__(self, rate=BATTERY_DEFAULT_RATE):
-        self._battery_status = BATTERY_FULL_PERCENTAGE
+        self._battery_status = BATTERY_FULL_PERCENTAGE # convention: prefix single underscore to non-publ. instan. vars
         self._sub = rospy.Subscriber('/drone/battery', Int32, callback=self.battery_status_callback)
         self._rate = rate
 
 
 if __name__ == '__main__':
     try:
-        rospy.init_node('battery_node')  # start node
+        rospy.init_node('battery_node')  # register the node with roscore, allowing it to communicate with other nodes
         battery_subscriber = BatterySubscriber()
         battery_subscriber.start()
     except rospy.ROSInterruptException:
