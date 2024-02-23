a87920bae774b83e7f13b1bb5b997d2bfa453ff0
Sebastian Moscicki <e0968493@student.tuwien.ac.at>
2024-02-23 19:17:22 +0100

    Control&Gazebo: Implemented action command class for movement with additional actions

--- control/scripts/control_node.py
+++ control/scripts/control_node.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python3
 import rospy
+from control.scripts.action.drone_action_command import DroneActionCommand
 from std_msgs.msg import String
 
 
 class DroneControl:
     def __init__(self):
         # Class Variables from different nodes
-        self.battery_data = None
+        self.drone_data = None
         self.planning_data = None
-        self.cv_data = None
+        self.mapping_data = None
         rospy.init_node("drone_control_node", anonymous=True)
 
-        rospy.Subscriber("cv_data", String, self.cv_callback)
+        rospy.Subscriber("mapping_data", String, self.cv_callback)
 
         rospy.Subscriber("planning_data", String, self.planning_callback)
 
-        rospy.Subscriber("battery_data", String, self.battery_callback)
+        self.drone_command_pub = rospy.Publisher("drone_node", String, queue_size=10)
 
-        self.tello_command_pub = rospy.Publisher("tello_data", String, queue_size=10)
+        self.drone_action_command = DroneActionCommand()
 
     def cv_callback(self, data):
         self.cv_data = data.data
 
-    def planning_callback(self, data):
-        self.planning_data = data
+    def planning_callback(self, planning_data):
+        for point in planning_data:
+            x1, y1, x2, y2, course, bear, dist = point
+            if bear == 0:
+                self.drone_action_command.send_action_command(dist, 0.0, 0.0, 0.0, 0.0, 0.0)
+            elif bear != 0:
+                self.drone_action_command.send_action_command(0.0, 0.0, 0.0, 0.0, 0.0, bear)
 
-    def battery_callback(self, data):
-        self.battery_data = data
+            ##TODO: Hier weitere Bedingunge hinzufügen
+
 
     def main_process(self):
         # 10 Hz Rate
@@ -36,16 +42,11 @@ class DroneControl:
 
             ## TODO: Implementation of the logic for executing control data to tello
 
-            if self.cv_data is not None:
+            if self.mapping_data is not None:
                 rospy.loginfo(f"Received cv_data: {self.cv_data}")
 
             if self.planning_data is not None:
                 rospy.loginfo(f"Received planning_data: {self.planning_data}")
-
-            if self.battery_data is not None:
-                rospy.loginfo(f"Received battery_data: {self.battery_data}")
-
-            self.tello_command_pub.publish("example_command")
 
             rate.sleep()
 
@@ -54,5 +55,6 @@ if __name__ == '__main__':
     try:
         control_node = DroneControl()
         control_node.main_process()
+        rospy.spin()
     except rospy.ROSInterruptException:
         pass
