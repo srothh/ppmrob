80ffc6f1574a59df93c2b97d8d50a023fb908d78
lazafi <lazafi@gmail.com>
2024-03-08 12:59:44 +0100

    project layout

--- src/drone/src/action/__init__.py
+++
@@ -1,4 +0,0 @@
-from .launch_action import LaunchAction
-from .emergency_action import EmergencyAction
-from .move_action import MoveAction
-from .command_action import CommandAction
--- src/drone_dummy/src/sensor/__init__.py
+++
@@ -1,3 +0,0 @@
-from .image_sensor import ImageSensor
-from .twist_sensor import TwistSensor
-from .battery_sensor import BatterySensor
