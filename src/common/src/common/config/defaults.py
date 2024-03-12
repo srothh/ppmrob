6b413395b53c78e687cd0e005d0d63596d7bec8a
Peter <vrabelpeter3618@gmail.com>
2024-03-12 18:51:46 +0100

    update default constants

--- src/common/src/common/config/defaults.py
+++ src/common/src/common/config/defaults.py
@@ -1,10 +1,29 @@
 drone_command_timeout = 30
-drone_twist_sensor_publish_topic_name = '/drone/twist'
-drone_image_sensor_publish_topic_name = '/drone/camera'
-drone_battery_sensor_publish_topic_name = '/drone/battery'
-drone_launch_action_name = 'launch'
-drone_move_action_name = 'move'
-drone_command_action_name = 'command'
-drone_emergency_action_name = 'emergency'
+drone_twist_sensor_publish_topic_name = "/drone/twist"
+drone_image_sensor_publish_topic_name = "/drone/camera"
+drone_battery_sensor_publish_topic_name = "/drone/battery"
+drone_launch_action_name = "launch"
+drone_move_action_name = "move"
+drone_command_action_name = "command"
+drone_emergency_action_name = "emergency"
 
-battery_publish_topic_name = '/battery/return_signal'
+battery_publish_topic_name = "/battery/return_signal"
+
+
+class Odometry:
+    HOME_COORDS_TOPIC_NAME = "/odometry/home_coordinates"
+
+
+class Mapping:
+    VICTIM_FOUND_TOPIC_NAME = "/mapping/victim_found"
+
+
+class Planning:
+    MOVE_ACTION_NAMESPACE = "planning_move"
+    COMMAND_ACTION_NAMESPACE = "planning_command"
+
+
+class TelloCommands:
+    TAKEOFF = "takeoff"
+    LAND = "land"
+    STOP = "stop"
