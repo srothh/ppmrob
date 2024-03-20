ae76057a54403e7e1bf62f6e71ea5bd9904ff737
DiamondXTech <74365025+DiamondXTech@users.noreply.github.com>
2024-03-20 16:45:16 +0100

    Small updates to mapping node

--- src/mapping/scripts/mapping_node.py
+++ src/mapping/scripts/mapping_node.py
@@ -1,8 +1,24 @@
 #!/usr/bin/env python3
 
 import rospy
+from geometry_msgs.msg import Polygon
 
-# TODO: mapping code
+class Map:
+    def __init__(self):
+        self.width = 100
+        self.height = 100
+
+    def update_map_size(self, width, height):
+        self.width = width
+        self.height = height
+        # TODO: Think about how to keep all the recent information!
+
+
+def line_callback(polygon):
+    p1 = polygon.points[0]
+    p2 = polygon.points[1]
+
+
 
 if __name__ == '__main__':
     rospy.init_node('mapping')
