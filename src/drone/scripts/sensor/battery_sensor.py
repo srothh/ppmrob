bc86b728af835ea5e4639e8d0acde8e8dda614b0
Peter <vrabelpeter3618@gmail.com>
2024-03-22 20:58:01 +0100

    adjust topic type and dependecies for planner

--- src/drone/scripts/sensor/battery_sensor.py
+++ src/drone/scripts/sensor/battery_sensor.py
@@ -1,25 +1,24 @@
 #!/usr/bin/env python3
 
 import rospy
-from std_msgs.msg import UInt8
+from sensor_msgs.msg import BatteryState
 
-class BatterySensor():
+
+class BatterySensor:
 
     _drone = None
 
     def __init__(self, drone=None, topic=None):
-        self._publisher = rospy.Publisher(topic, UInt8, queue_size=10)
-        self._drone = drone        
+        self._publisher = rospy.Publisher(topic, BatteryState, queue_size=10)
+        self._drone = drone
         self._counter = 0
 
     def publish(self, event=None, state=None):
         try:
             if state:
-                msg = UInt8()
-                msg.data = state.get('bat')
+                msg = BatteryState()
+                msg.percentage = state.get("bat")
                 self._publisher.publish(msg)
                 self._counter += 1
         except Exception as e:
             rospy.loginfo(e)
-    
-
