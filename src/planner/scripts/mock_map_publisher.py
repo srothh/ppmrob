0aa434e3154971921dbf69268819b7f3e225bc63
Peter <vrabelpeter3618@gmail.com>
2024-04-10 23:13:24 +0200

    comment out annoying

--- src/planner/scripts/mock_map_publisher.py
+++ src/planner/scripts/mock_map_publisher.py
@@ -5,9 +5,12 @@ from std_msgs.msg import Header
 
 import common.config.defaults as defaults  # TODO add to dockerfile as per issue!
 
+
 def publish_occupancy_grid():
-    rospy.init_node('mock_occupancy_grid_publisher', anonymous=True)
-    pub = rospy.Publisher(defaults.Mapping.OCCUPANCY_GRID_TOPIC_NAME, OccupancyGrid, queue_size=10)
+    rospy.init_node("mock_occupancy_grid_publisher", anonymous=True)
+    pub = rospy.Publisher(
+        defaults.Mapping.OCCUPANCY_GRID_TOPIC_NAME, OccupancyGrid, queue_size=10
+    )
     rate = rospy.Rate(1)  # 1 Hz
     while not rospy.is_shutdown():
         # Create a dummy occupancy grid
@@ -26,24 +29,115 @@ def publish_occupancy_grid():
         # Fill the grid with zeros (free space)
         # grid.data = [0] * 100  # 10x10 grid
         grid.data = [
-            100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-            100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
-            100, 100,   0,   0,   0,   0,   0,   0,   0, 100,
-            100, 100,   0, 100, 100, 100, 100,   1, 100, 100,
-            100, 100,   0, 100, 100, 100, 100, 100, 100, 100,
-            100, 100,   0, 100, 100, 100, 100, 100, 100, 100,
-            100, 100,   0, 100, 100, 100, 100, 100, 100, 100,
-            100, 100,   0, 100, 100, 100, 100, 100, 100, 100,
-            100, 100,   0, 100, 100, 100, 100, 100, 100, 100,
-            100, 100, 100, 100, 100, 100, 100, 100, 100, 100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+            100,
+            100,
+            100,
+            0,
+            100,
+            100,
+            100,
+            100,
+            1,
+            100,
+            100,
+            100,
+            100,
+            0,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            0,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            0,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            0,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            0,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
+            100,
         ]
-
 
         # Publish the occupancy grid
         pub.publish(grid)
-        rospy.loginfo("Published mock occupancy grid")
+        # rospy.loginfo("Published mock occupancy grid")
         rate.sleep()
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     try:
         publish_occupancy_grid()
     except rospy.ROSInterruptException:
